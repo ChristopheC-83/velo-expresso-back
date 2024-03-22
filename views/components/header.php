3fc53487872c2c9994bfe9a0884092c16097c8f3
ChristopheC83 <christophe.chiappetta@gmail.com>
2024-03-22 15:24:40 +0100

    création des fichiers

--- views/components/header.php
+++ views/components/header.php
@@ -10,9 +10,9 @@
 <?php if (!empty($_SESSION['user_name'])) : ?>
     <div class="min-vw-100 p-1 bg-info">
         <div class="container d-flex justify-content-between flex-column flex-sm-row">
-            <div class="m-0 ">
+            <div class="d-flex flex-column justify-content-between  m-0 ">
                 <h3 class="w-100 text-black text-center m-0">Accueil</h3>
-                <div class="container d-flex justify-content-center">
+                <div class=" w-100 d-flex flex-wrap justify-content-center">
                     <button class="btn btn-secondary btn-lg text-light m-1">
                         <a href="<?= URL ?>" class="text-light text-decoration-none">Slider</a>
                     </button>
@@ -23,7 +23,7 @@
                         <a href="<?= URL ?>" class="text-light text-decoration-none">Partenaires</a>
                     </button>
                     <button class="btn btn-secondary btn-lg text-light m-1">
-                        <a href="<?=URL?>" class="text-light text-decoration-none">Avis</a>
+                        <a href="<?= URL ?>" class="text-light text-decoration-none">Avis</a>
                     </button>
                 </div>
             </div>
@@ -33,18 +33,18 @@
                     <button class="btn btn-secondary btn-lg text-light m-1">
                         <a href="<?= URL ?>hangman/words_list" class="text-light text-decoration-none">Location</a>
                     </button>
-                    
+
                 </div>
             </div>
         </div>
         <div class="container d-flex justify-content-between flex-column flex-sm-row">
-        <div class="m-0 ">
+            <div class="m-0 ">
                 <h3 class="w-100 text-black text-center m-0">Atelier</h3>
                 <div class="container d-flex justify-content-center">
                     <button class="btn btn-secondary btn-lg text-light m-1">
                         <a href="<?= URL ?>hangman/words_list" class="text-light text-decoration-none">Cat / Presta</a>
                     </button>
-                    
+
                 </div>
             </div>
             <div class="m-0">
@@ -56,10 +56,21 @@
                     <button class="btn btn-secondary btn-lg text-light m-1">
                         <a href="<?= URL ?>" class="text-light text-decoration-none">Vélos</a>
                     </button>
-                   
                 </div>
             </div>
-            
+
         </div>
     </div>
+
+    <div class="my-2 w-100 h-100 p-1 p-sm-2 p-md-3">
+        <h2 class="text-center mb-2"><u>Notice :</u></h2>
+        <h3><u>Accueil:</u></h3>
+        <h3><u>Location:</u></h3>
+        <h3><u>Atelier:</u></h3>
+        <h3><u>Vélos:</u></h3>
+
+
+
+    </div>
+
 <?php endif ?>
