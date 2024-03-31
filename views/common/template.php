4659a79547e709049e204043ad4f429c5d91788d
ChristopheC83 <christophe.chiappetta@gmail.com>
2024-03-31 14:01:48 +0200

    purge css

--- views/common/template.php
+++ views/common/template.php
@@ -7,8 +7,8 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=roboto:100,400,700,900" rel="stylesheet" />
-    <link href="<?= URL ?>public/style/style.css" rel="stylesheet" />
-    <!-- <link href="<?= URL ?>public/css/cleaned_css/final_css.css" rel="stylesheet" /> -->
+    <!-- <link href="<?= URL ?>public/style/style.css" rel="stylesheet" /> -->
+    <link href="<?= URL ?>public/style/cleaned/final_css.css" rel="stylesheet" />
 
      <!-- tiny -->
      <script src="https://cdn.tiny.cloud/1/ioe3eh75wlk0ed6bxqzlm9mmphf2fa5dzibxjrx77bxl9520/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
